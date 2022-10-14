ed6927c96718b558840c59b737946ea94bbd0e13
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-10-14 16:05:59 -0300

    agregado de CoreEscuela y platziHelloWorld

---
+++ CoreEscuela/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class CoreEscuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundación;
+        public string ceo;
+
+        public void Timbrar()
+        {
+            for (int i = 800; i < (800 + 500 * 5); i += 500)
+            {
+                Console.Beep(i, 500);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new CoreEscuela();
+
+            miEscuela.nombre = "Platzi Academy";
+            miEscuela.direccion = "Cr 9 calle 72";
+            miEscuela.añoFundación = 2012;
+
+            Console.WriteLine("TIMBRE");
+            
+            miEscuela.Timbrar();
+        }
+    }
+}
